cd440fe9e1fcfe77d235d303158b47d9f7905679
Mateus Lucena <mateuslucena2014@gmail.com>
2025-04-22 00:11:07 -0300

    Create Database and Table

--- MyHabitTracker/Program.cs
+++ MyHabitTracker/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+namespace MyHabitTracker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = @"Data Source=MyHabitTracker.db";
+
+        using var connection = new SqliteConnection(connectionString);
+        connection.Open();
+        var tableCmd = connection.CreateCommand();
+
+        //Using @ allow to create multiline statement
+        tableCmd.CommandText =
+            @"CREATE TABLE IF NOT EXISTS drinking_water (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT NOT NULL,
+                    Quantity INTEGER NOT NULL
+                    )";
+
+        tableCmd.ExecuteNonQuery();
+
+        connection.Close();
+    }
+}
