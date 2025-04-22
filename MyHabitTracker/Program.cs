27de5bbfbce5a27abbec034f555f3a1f8ab475d3
Mateus Lucena <mateuslucena2014@gmail.com>
2025-04-22 12:21:16 -0300

    insert and view all methods

--- MyHabitTracker/Program.cs
+++ MyHabitTracker/Program.cs
@@ -1,28 +1,33 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using System.Globalization;
+using Microsoft.Data.Sqlite;
 
 namespace MyHabitTracker;
 
 class Program
 {
+    static string _connectionString = @"Data Source=MyHabitTracker.db";
+
     static void Main(string[] args)
     {
-        string connectionString = @"Data Source=MyHabitTracker.db";
-
-        using var connection = new SqliteConnection(connectionString);
+        using var connection = new SqliteConnection(_connectionString);
         connection.Open();
         var tableCmd = connection.CreateCommand();
 
         //Using @ allow to create multiline statement
         tableCmd.CommandText =
-            @"CREATE TABLE IF NOT EXISTS drinking_water (
-                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    Date TEXT NOT NULL,
-                    Quantity INTEGER NOT NULL
-                    )";
+            """
+            CREATE TABLE IF NOT EXISTS drinking_water (
+                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Date TEXT NOT NULL,
+                                Quantity INTEGER NOT NULL
+                                )
+            """;
 
         tableCmd.ExecuteNonQuery();
 
         connection.Close();
+
+        GetUserInput();
     }
 
     static void GetUserInput()
@@ -55,10 +60,10 @@ class Program
                     Insert();
                     break;
                 case "3":
-                    Delete();
+                    //Delete();
                     break;
                 case "4":
-                    Update();
+                    //Update();
                     break;
                 default:
                     System.Console.WriteLine("\nInvalid Command. Write a number from 0 to 4.\n");
@@ -66,4 +71,93 @@ class Program
             }
         }
     }
+
+    private static void Insert()
+    {
+        string date = GetDateInput();
+
+        int quantity = GetNumberInput("\n\nPlease insert number of glasses or other measure of your choice (no decimals allowed)\n\n");
+
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+        var tableCmd = connection.CreateCommand();
+        tableCmd.CommandText = 
+            $"INSERT INTO drinking_water(date, quantity) VALUES('{date}', {quantity})";
+
+        tableCmd.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    internal static string GetDateInput()
+    {
+        System.Console.WriteLine("\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to main menu.\n");
+
+        string? dateInput = Console.ReadLine();
+
+        if (dateInput == "0") GetUserInput();
+
+        return dateInput;
+    }
+
+    internal static int GetNumberInput(string message)
+    {
+        System.Console.WriteLine(message);
+
+        string? numberInput = Console.ReadLine();
+
+        if (numberInput == "0") GetUserInput();
+
+        int finalInput = Convert.ToInt32(numberInput);
+
+        return finalInput;
+    }
+
+    internal static void GetAllRecords()
+    {
+        Console.Clear();
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+        var tableCmd = connection.CreateCommand();
+        tableCmd.CommandText = $"SELECT * FROM drinking_water";
+
+        List<DrinkingWater> tableData = new();
+
+        SqliteDataReader reader = tableCmd.ExecuteReader();
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                tableData.Add(
+                    new DrinkingWater
+                    {
+                        Id = reader.GetInt32(0),
+                        Date = DateTime.ParseExact(reader.GetString(1),"dd-mm-yy",new CultureInfo("en-US")),
+                        Quantity = reader.GetInt32(2)
+                    }
+                );
+            }
+        }
+        else
+        {
+            Console.WriteLine("No rows found.");
+        }
+        
+        connection.Close();
+
+        Console.WriteLine("---------------------------\n");
+        foreach (var dw in tableData)
+        {
+            Console.WriteLine($"{dw.Id} - {dw.Date.ToString("dd-MM-yyyy")} - Quantidade: {dw.Quantity}");
+        }
+
+        Console.WriteLine("---------------------------\n");
+    }
+}
+
+public class DrinkingWater
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int Quantity { get; set; }
 }
