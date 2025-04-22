f90bb264b23847514f684fb00a166003d7277725
Mateus Lucena <mateuslucena2014@gmail.com>
2025-04-22 11:12:36 -0300

    create main menu

--- MyHabitTracker/Program.cs
+++ MyHabitTracker/Program.cs
@@ -24,4 +24,46 @@ class Program
 
         connection.Close();
     }
+
+    static void GetUserInput()
+    {
+        Console.Clear();
+        bool closeApp = false;
+        while (closeApp == false)
+        {
+            System.Console.WriteLine("\n\nMAIN MENU");
+            System.Console.WriteLine("\nWhat would you like to do?");
+            System.Console.WriteLine("\nType 0 to Close Application.");
+            System.Console.WriteLine("Type 1 to View All Record.");
+            System.Console.WriteLine("Type 2 to Insert Record.");
+            System.Console.WriteLine("Type 3 to Delete Record.");
+            System.Console.WriteLine("Type 4 to Update Record.");
+            System.Console.WriteLine("-------------------------------------\n");
+
+            string? commandInput = Console.ReadLine();
+
+            switch(commandInput)
+            {
+                case "0":
+                    System.Console.WriteLine("\nGoodbye\n");
+                    closeApp = true;
+                    break;
+                case "1":
+                    GetAllRecords();
+                    break;
+                case "2":
+                    Insert();
+                    break;
+                case "3":
+                    Delete();
+                    break;
+                case "4":
+                    Update();
+                    break;
+                default:
+                    System.Console.WriteLine("\nInvalid Command. Write a number from 0 to 4.\n");
+                    break;
+            }
+        }
+    }
 }
